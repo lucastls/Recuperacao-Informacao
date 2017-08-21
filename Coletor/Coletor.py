c998ecb3d7edd15f2d72981e143bd9f7c1c71b4b
Lucas Santos <lucastls@outlook.com>
2017-08-21 19:06:58 -0300

    Verificação de erros HTTP

--- Coletor/Coletor.py
+++ Coletor/Coletor.py
@@ -2,20 +2,28 @@ from lxml import etree, cssselect, html
 from urllib.error import URLError, HTTPError, ContentTooShortError
 import urllib.request
 
-url = 'https://www.ratebeer.com/breweries/brazil/0/31/'
+url = 'https://www.ign.com/'
 
-def Obter_links(url):
+def Download_HTML(url, num_tentativas):
 
 	htmldoc = urllib.request.Request(url)
 	htmldoc.add_header('User-agent', 'elmbot')
 
 	try:
-		Links.pop()
-
-	try:
-		htmldoc = urllib.request.urlopen(htmldoc).read()
+		htmldoc = urllib.request.urlopen(htmldoc).read().decode()
 	except (URLError, HTTPError, ContentTooShortError) as error:
 		print('Erro: ',error)
+		html = None
+		if num_tentativas > 0:
+			if hasattr(error, 'code') and (500 <= error.code < 600):
+				# recursivavmente tenta de novo para erros HTTP 5xx
+				return Obter_links(url, num_retries - 1)
+
+	return htmldoc
+
+def Obter_links(url):
+
+	htmldoc = Download_HTML(url,2)
 
 	tree = html.fromstring(htmldoc) # parse the HTML and fixes it
 	htmldoc = html.tostring(tree, pretty_print=True)
@@ -24,12 +32,15 @@ def Obter_links(url):
 	get_links = cssselect.CSSSelector('a')
 	Links = [ link.get('href') for link in get_links(htmldoc)]
 
-	dominio=url[:len(url)-1]
+	pagRaiz = url.split(':')[0] + '://' + url.split('//')[1].split('/')[0] + '/'
 
-	print(Links)
+	dominio = url.split('.')[1].split('.')[0]
 
 	for i in range(len(Links)):
 		if Links[i] and Links[i].startswith('/'):
-			Links[i]=dominio+Links[i]
-
+			Links[i]=pagRaiz+Links[i]
+		if Links[i] and Links[i].startswith('#'):
+			del Links[i]
 	return Links
+
+Obter_links(url)
